24b345a3652fd5574f523707afa39894dfbce28a
Jhalaboi1121 <chmoo272@learner.dvusd.org>
2024-12-03 12:30:26 -0700

    Added first picture

---
+++ Tetris/Tetris/Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Timer1 = New Timer(components)
+        Avatar = New PictureBox()
+        Enemy = New PictureBox()
+        Enemy2 = New PictureBox()
+        Ring1 = New PictureBox()
+        CType(Avatar, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Enemy, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Enemy2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Ring1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Enabled = True
+        ' 
+        ' Avatar
+        ' 
+        Avatar.Image = CType(resources.GetObject("Avatar.Image"), Image)
+        Avatar.Location = New Point(113, 68)
+        Avatar.Name = "Avatar"
+        Avatar.Size = New Size(125, 94)
+        Avatar.SizeMode = PictureBoxSizeMode.StretchImage
+        Avatar.TabIndex = 0
+        Avatar.TabStop = False
+        ' 
+        ' Enemy
+        ' 
+        Enemy.Image = CType(resources.GetObject("Enemy.Image"), Image)
+        Enemy.Location = New Point(632, 68)
+        Enemy.Name = "Enemy"
+        Enemy.Size = New Size(107, 82)
+        Enemy.SizeMode = PictureBoxSizeMode.StretchImage
+        Enemy.TabIndex = 3
+        Enemy.TabStop = False
+        ' 
+        ' Enemy2
+        ' 
+        Enemy2.Image = CType(resources.GetObject("Enemy2.Image"), Image)
+        Enemy2.Location = New Point(579, 180)
+        Enemy2.Name = "Enemy2"
+        Enemy2.Size = New Size(107, 82)
+        Enemy2.SizeMode = PictureBoxSizeMode.StretchImage
+        Enemy2.TabIndex = 15
+        Enemy2.TabStop = False
+        ' 
+        ' Ring1
+        ' 
+        Ring1.Image = CType(resources.GetObject("Ring1.Image"), Image)
+        Ring1.Location = New Point(306, 100)
+        Ring1.Name = "Ring1"
+        Ring1.Size = New Size(63, 62)
+        Ring1.SizeMode = PictureBoxSizeMode.StretchImage
+        Ring1.TabIndex = 16
+        Ring1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(944, 398)
+        Controls.Add(Ring1)
+        Controls.Add(Enemy2)
+        Controls.Add(Enemy)
+        Controls.Add(Avatar)
+        Name = "Form1"
+        Text = "Form1"
+        CType(Avatar, ComponentModel.ISupportInitialize).EndInit()
+        CType(Enemy, ComponentModel.ISupportInitialize).EndInit()
+        CType(Enemy2, ComponentModel.ISupportInitialize).EndInit()
+        CType(Ring1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Avatar As PictureBox
+    Friend WithEvents Enemy As PictureBox
+    Friend WithEvents Enemy2 As PictureBox
+    Friend WithEvents Ring1 As PictureBox
+
+End Class
