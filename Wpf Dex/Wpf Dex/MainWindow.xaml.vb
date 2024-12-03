f5a33ca92be3f200e7df0f186d12660d2968052f
Jhalaboi1121 <chmoo272@learner.dvusd.org>
2024-12-03 13:06:18 -0700

    Ruhehehehe

--- Wpf Dex/Wpf Dex/MainWindow.xaml.vb
+++ Wpf Dex/Wpf Dex/MainWindow.xaml.vb
@@ -1,3 +1,5 @@
 ﻿Class MainWindow
-
+    Private Sub Button1_Click(sender As Object, e As RoutedEventArgs) Handles Button1.Click
+        DeathCrossbow.Visibility = Visibility.Hidden
+    End Sub
 End Class
